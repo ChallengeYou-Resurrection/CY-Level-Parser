eb123c245bbfc013310ec02e2c48cda835e4448b
Hopson97 <mhopson@hotmail.co.uk>
2018-12-20 20:28:14 +0000

    Add split function

--- src/Utilities.h
+++ src/Utilities.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 /**
  * @brief Get the length of a file, useful for optimization purposes for allocating
@@ -18,3 +19,11 @@ size_t getFileLength(const char* fileName);
  * @return std::string String containing everything inside of this file
  */
 std::string getFileContent(const char* fileName);
+
+/**
+ * @brief Splits a string at a deliminator into seperate tokens
+ * 
+ * @param deliminator Character to split the string at
+ * @return std::vector<std::string> The tokens
+ */
+std::vector<std::string> split(char deliminator);
