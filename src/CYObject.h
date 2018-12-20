922f9b12ae5840286869dfda3b3d6948b7bf89be
Hopson97 <mhopson@hotmail.co.uk>
2018-12-20 20:13:25 +0000

    Add some docs

--- src/CYObject.h
+++ src/CYObject.h
@@ -2,11 +2,19 @@
 
 #include <string>
 
+/**
+ * @brief X/Z coordinates
+ * 
+ */
 struct Position {
     int x = 0;
-    int y = 0;
+    int z = 0;
 };
 
+/**
+ * @brief Represents a single CY Object
+ * 
+ */
 struct CYObject {
     Position position; 
     std::string properties;
--- src/FileParser.h
+++ src/FileParser.h
@@ -5,6 +5,15 @@
 
 #include "CYObject.h"
 
+/**
+ * @brief Type alias for a map containing information about all the objects in a level
+ */
 using CYObjectMap = std::unordered_map<std::string, std::vector<CYObject>>;
 
+/**
+ * @brief Parses the CY Level format to extract the data from it
+ * 
+ * @param fileName The name of the .cy file to parse
+ * @return CYObjectMap A map containing all the information about the CYObjects in this level
+ */
 CYObjectMap parseFile(const char* fileName);
