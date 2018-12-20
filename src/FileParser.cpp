99c71a8f5597491387178203b65a7b69c5b21ffc
Hopson97 <mhopson@hotmail.co.uk>
2018-12-20 20:53:46 +0000

    Extract level metadata

--- src/FileParser.cpp
+++ src/FileParser.cpp
@@ -3,14 +3,22 @@
 
 #include <iostream>
 
+namespace {
+    std::string getMetadataAttribute(
+        const std::string& name, const std::string& token, bool isString) {
+        return token.substr(
+            name.length() + (isString ? 3 : 2),
+            token.length() - name.length() - (isString ? 6 : 4));
+    }
+}
 CYObjectMap parseFile(const char* fileName) {
     CYObjectMap objects;
-    auto content = getFileContent(fileName);
+    auto tokens = split(getFileContent(fileName), '#');
 
-    auto tokens = split(content, '#');
-    for (auto& t : tokens) {
-        std::cout << t << '\n';
-    }
+    auto name       = getMetadataAttribute("name", tokens[1], true);
+    auto floors     = getMetadataAttribute("levels", tokens[2], false);
+    auto version    = getMetadataAttribute("version", tokens[3], false);
+    auto creator    = getMetadataAttribute("creator", tokens[4], true);
     
     return objects;
 }
