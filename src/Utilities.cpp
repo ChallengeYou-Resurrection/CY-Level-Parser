57ed8d063bf6eddc85bd4a682e67b7d654f36951
Hopson97 <mhopson@hotmail.co.uk>
2018-12-21 20:47:00 +0000

    Remove code comment

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -10,25 +10,14 @@ size_t getFileLength(const char* fileName) {
 
 std::string getFileContent(const char* fileName) {
     std::fstream inFile(fileName);
+    if (!inFile.is_open()) return "";
     std::string fileContent;
-    fileContent.reserve(getFileLength(fileName));
+    auto length = getFileLength(fileName);
+    fileContent.reserve(length);
     std::getline(inFile, fileContent);
     return fileContent;
 }
-/*
-std::vector<std::string> split(const std::string& string, char deliminator) {
-    std::vector<std::string> tokens;
-    std::string token = "";
-    for (auto c : string) {
-        if (c == deliminator) {
-            tokens.push_back(std::move(token));
-            continue;
-        }
-        token.push_back(c);
-    }
-    tokens.push_back(std::move(token));
-    return tokens;
-}*/
+
 
 std::optional<size_t> indexOf(const std::string& string, char token) {
     for (size_t i = 0; i < string.length(); i++) {
