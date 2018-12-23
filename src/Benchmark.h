de39ac691c51031cece53c4bc31a4fe122fb4195
Hopson97 <mhopson@hotmail.co.uk>
2018-12-23 22:34:45 +0000

    Return back to old string substring code rather than using string_views as substring seems to be twice as fast somehoe

--- src/Benchmark.h
+++ src/Benchmark.h
@@ -8,52 +8,76 @@
 #include <algorithm>
 #include <cmath>
 
+namespace benchmark {
 
-template<int N>
-class Benchmark {
     using Clock = std::chrono::high_resolution_clock;
     using TimeUnit    = std::chrono::microseconds;
     using UnitType  = std::int64_t;
-    public:
-        template <typename F, typename... Args>
-        Benchmark(const char* name, F function, Args... args)
-        :m_name(name) 
-        { 
-            UnitType total = 0;
-            std::array<UnitType, N> times;
 
-            for (int i = 0; i < N; i++) {
-                auto begin = Clock::now();
-                function(std::forward<Args>(args)...);
-                auto end = Clock::now();
-                auto time = end - begin;
-                auto ms = std::chrono::duration_cast<TimeUnit>(time);
-                total += ms.count();
-                times[i] = ms.count();
+    template<typename C = Clock>
+    class Timer {
+        using TimePoint = typename C::time_point;
+        public:
+            Timer() {
+                reset();
             }
-            m_totalTime = total;
-            m_avgTime   = (float(total) / float(N));
-            m_minTime   = (*std::min_element(times.cbegin(), times.cend()));
-            m_maxTime   = (*std::max_element(times.cbegin(), times.cend()));
-        }
+
+            void reset() {
+                start = C::now();
+            }
+
+            void printElapsed() {
+                auto endTime = C::now();
+                std::cout << std::chrono::duration_cast<TimeUnit>(endTime - start).count() / 1000.0 << "ms" << '\n';
+            }
+
+        private:
+            TimePoint start;
+    };
+
+    template<int N>
+    class Benchmark {
+        public:
+            template <typename F, typename... Args>
+            Benchmark(const char* name, F function, Args... args)
+            :m_name(name) 
+            { 
+                UnitType total = 0;
+                std::array<UnitType, N> times;
+
+                for (int i = 0; i < N; i++) {
+                    auto begin = Clock::now();
+                    function(std::forward<Args>(args)...);
+                    auto end = Clock::now();
+                    auto time = end - begin;
+                    auto ms = std::chrono::duration_cast<TimeUnit>(time);
+                    total += ms.count();
+                    times[i] = ms.count();
+                }
+                m_totalTime = total;
+                m_avgTime   = (float(total) / float(N));
+                m_minTime   = (*std::min_element(times.cbegin(), times.cend()));
+                m_maxTime   = (*std::max_element(times.cbegin(), times.cend()));
+            }
 
 
-        void outputTimes() {
-            std::cout << "\n==================================\n";
-            std::cout << "Results for benchmark: "  << m_name       << '\n';
-            std::cout << "Times benchmarked: "      << N      << "\n\n";
-           // std::cout << "  Total time: "           << m_totalTime  << "ms\n";
-            std::cout << "Average time: "           << m_avgTime / 1000.0    << "ms\n";
-            std::cout << "Minimum time: "           << m_minTime / 1000.0   << "ms\n";
-            std::cout << "Maximum time: "           << m_maxTime / 1000.0   << "ms\n";
-            std::cout << "       Range: "           << (m_maxTime - m_minTime) / 1000.0 << "ms\n";
-            std::cout << "==================================\n";
-        }
-    private:
+            void outputTimes() {
+                std::cout << "\n==================================\n";
+                std::cout << "Results for benchmark: "  << m_name       << '\n';
+                std::cout << "Times benchmarked: "      << N      << "\n\n";
+            // std::cout << "  Total time: "           << m_totalTime  << "ms\n";
+                std::cout << "Average time: "           << m_avgTime / 1000.0    << "ms\n";
+                std::cout << "Minimum time: "           << m_minTime / 1000.0   << "ms\n";
+                std::cout << "Maximum time: "           << m_maxTime / 1000.0   << "ms\n";
+                std::cout << "       Range: "           << (m_maxTime - m_minTime) / 1000.0 << "ms\n";
+                std::cout << "==================================\n";
+            }
+        private:
 
-        const char* m_name;
-        double m_totalTime;
-        double m_avgTime;
-        double m_minTime;
-        double m_maxTime;
-};
+            const char* m_name;
+            double m_totalTime;
+            double m_avgTime;
+            double m_minTime;
+            double m_maxTime;
+    };
+}
