e2b4341b47567769c6f0deb71f2ef57e55d0fc37
Hopson97 <mhopson@hotmail.co.uk>
2018-12-24 16:39:17 +0000

    begin work on binary file write

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,24 @@
 
 namespace fs = std::filesystem;
 
+//#define USE_SAMPLE_GAMES 
+#ifdef USE_SAMPLE_GAMES
+    #define SINGLE_GAME
+    #ifdef SINGLE_GAME
+        #define DIR "../../single"
+        #define OUT "../../single_out/"
+    #else
+        #define DIR "../../sample"
+        #define OUT "../../sample_out/"
+    #endif
+#else 
+    #define DIR "../../Games"
+    #define OUT "../../out/"
+#endif
+
 //This is tempory for testing, will be changed later
 void writeLevel(const std::string& name, const CYLevel& level) {
-    std::ofstream outfile("../../out/" + name + ".out");
+    std::ofstream outfile(OUT + name + ".out");
     outfile << "Name:    "  << level.name           << '\n' 
             << "Author:  "  << level.creator        << '\n' 
             << "Version: "  << level.version        << '\n'
@@ -45,7 +60,7 @@ void writeLevel(const std::string& name, const CYLevel& level) {
     }
     
     for (const auto& cyObject : level.objects) {
-        outfile << cyObject.first << '\n';
+        outfile << "Object ID: " << (int)cyObject.first << '\n';
         for (const auto& obj : cyObject.second) {
             outfile << "\tPosition:   " << obj.position.x << " " << obj.position.z << '\n'
                     << "\tFloor:      " << (int)obj.floor << '\n'
@@ -56,6 +71,26 @@ void writeLevel(const std::string& name, const CYLevel& level) {
             outfile << "\n\n";
         }
     }
+}
+
+void writeUint8(uint8_t n, std::ofstream& outFile) {
+    outFile.write(reinterpret_cast<const char*>(&n), sizeof(n));
+}
+
+void writeString(const std::string& string, std::ofstream& outFile) {
+    uint8_t size = (uint8_t)string.size();
+    outFile.write(reinterpret_cast<const char*>(&size), sizeof(size));
+    outFile.write(string.c_str(), string.size());
+}
+
+void writeLevelBinary(const std::string& name, const CYLevel& level) {
+    std::ofstream outfile(OUT + name + ".cyb", std::ofstream::binary);
+    writeString(level.name, outfile);
+    writeString(level.creator, outfile);
+    writeUint8((uint8_t)std::stoi(level.numFloors), outfile);
+    writeUint8((uint8_t)level.backmusic, outfile);
+    writeUint8((uint8_t)level.theme, outfile);
+    writeUint8((uint8_t)level.weather, outfile); 
 }
 
 std::optional<CYLevel> readFile(const std::string& name) {
@@ -72,34 +107,33 @@ void testLocal(const std::string& name) {
 }
 
 auto getGamesDirectoryItr() {
-    return fs::directory_iterator("../../Games");
+    return fs::directory_iterator(DIR);
 }
 
 int main() {
+    #ifndef USE_SAMPLE_GAMES
     benchmark::Timer<> timer;
     const int percentIncrement = std::distance(getGamesDirectoryItr(), fs::directory_iterator{}) / 100;
     int count = 0;
     int progress = 0;
     std::cout << "Total files: " << percentIncrement * 100 << std::endl;
     timer.reset();
+    #endif
     for (const auto& path : getGamesDirectoryItr()) {
+        #ifndef USE_SAMPLE_GAMES
         if (++count % percentIncrement == 0) {
             printf("Progress: %d%% [%d out of %d games converted] ", ++progress, count, percentIncrement * 100);
             printf("[Time: %fms]\n", timer.getTime());
             timer.reset();
         }
-        try {
-            const std::string name = path.path().filename().string();
-            auto level = readFile(path.path().c_str());
-            if (level) {
-                writeLevel(name, *level);
-            }   
-        }
-        catch (std::exception& e) {
-            std::cout << "Cannot parse: " << path.path().string() << '\n';
-            std::cout << e.what() << '\n';
-            return 0;
-        }
+        #endif
+
+        const std::string name = path.path().filename().string();
+        auto level = readFile(path.path().c_str());
+        if (level) {
+            writeLevelBinary(name, *level);
+            writeLevel(name, *level);
+        }   
     }
     printErrors();
     return 0;
