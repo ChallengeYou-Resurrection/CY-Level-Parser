a5344770a4fe7742183f7daa6a9e509b227a0ec4
Hopson97 <mhopson@hotmail.co.uk>
2018-12-24 07:25:50 +0000

    Begin work on colour conversioN

--- src/TextureConverter.cpp
+++ src/TextureConverter.cpp
@@ -1,10 +1,50 @@
 #include "TextureConverter.h"
 
 #include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <iostream>
 
+#include "Utilities.h"
+
+namespace {
+    using ConvertFunction = std::function<int32_t(const std::string&)>;
+
+    bool isColourTexture(const std::string& texture) {
+        return texture.find("color") != std::string::npos;
+    }
+
+    int32_t getColourInteger(const std::string& texture) {
+        auto colours = split(texture, ',');
+        colours[0] = colours[0].substr(6);
+        colours[2].pop_back();
+
+        auto r = std::stoi(colours[0]);
+        auto g = std::stoi(colours[1]);
+        auto b = std::stoi(colours[2]);
+        
+        return r << 24 | g << 16 | b;
+    }
+
+    int32_t convertWallTexture(const std::string& texture) {
+        if (isColourTexture(texture)) {
+            return getColourInteger(texture);
+        } 
+
+        return TextureID::None;
+    }
+
+    int32_t convertFloorTexture(const std::string& texture) {
+        if (isColourTexture(texture)) {
+            return getColourInteger(texture);
+        } 
+
+        return TextureID::None;
+    }
+}
 
 bool hasTexture(const std::string& texture) {
-    static const std::unordered_set<std::string> objectsWithTextures = {
+    static const std::unordered_set<const std::string> objectsWithTextures = {
         "walls",
         "floor",
         "plat",
@@ -19,8 +59,11 @@ bool hasTexture(const std::string& texture) {
 }
 
 
-TextureID convertTexture(const std::string& object, const std::string& tex) {
+int32_t convertTexture(const std::string& object, const std::string& tex) {
+    const static std::unordered_map<const std::string, ConvertFunction> converter {
+        {"walls", &convertWallTexture},
+        {"floor", &convertFloorTexture},
+    };
 
-
-    return TextureID::None;
+    return converter.at(object)(tex);
 }
--- src/TextureConverter.h
+++ src/TextureConverter.h
@@ -2,29 +2,31 @@
 
 #include <string>
 
-enum class TextureID : uint8_t
-{
-	// Normal Textures
-	Color = 0,
-	Grass = 1,
-	Stucco = 2,
-	Brick = 3,
-	Stone = 4,
-	Wood = 5,
-	Happy = 6,
-	Egypt = 7,
-	Bark = 8,
-	Scifi = 9,
-	Tile = 10,
-	Rock = 11,
-	Parquet = 12,
-	Books = 13,
+//This is not an enum class because of colour textures are represented using 24bits for RGB
+//Layout for colours: 00000000RRRRRRRRGGGGGGGGBBBBBBBB
+namespace TextureID {
+    enum {
+        // Normal Textures
+        Grass = 1,
+        Stucco = 2,
+        Brick = 3,
+        Stone = 4,
+        Wood = 5,
+        Happy = 6,
+        Egypt = 7,
+        Bark = 8,
+        Scifi = 9,
+        Tile = 10,
+        Rock = 11,
+        Parquet = 12,
+        Books = 13,
 
-	// Translucent Textures
-	Bars = 14,
-	Glass = 15,
-    None,
-};
+        // Translucent Textures
+        Bars = 14,
+        Glass = 15,
+        None = 16,
+    };
+}
 
 bool hasTexture(const std::string& texture);
-TextureID convertTexture(const std::string& object, const std::string& texutre);
+int convertTexture(const std::string& object, const std::string& texutre);
