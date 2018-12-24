d48a6e11ce8a1509236293b420871a94e8afeedb
Hopson97 <mhopson@hotmail.co.uk>
2018-12-24 11:06:27 +0000

    Add CYObjectID enum

--- src/CYObject.h
+++ src/CYObject.h
@@ -5,6 +5,33 @@
 #include <map>
 #include <vector>
 
+enum class ObjectID : uint8_t {
+    Wall        = 0,
+    Floor       = 1,
+    Platform    = 2,
+    TriPlatform = 3,
+    DiaPlatform = 4,
+    Ramp        = 5,
+    TriWall     = 6,
+    Pillar      = 7,
+    Door        = 8,
+    Diamond     = 9,
+    Iceman      = 10,
+    Chaser      = 11,
+    Hole        = 12,
+    Start       = 13,
+    Finish      = 14,
+    JetPack     = 15,
+    Fuel        = 16,
+    Shield      = 17,
+    Slingshot   = 19,
+    Crumbs      = 20,
+    Teleport    = 21,
+    Key         = 22,
+    Ladder      = 23,
+    Portal      = 24,    
+};
+
 /**
  * @brief X/Z coordinates
  * 
