95d43832606ccf9979dcb28d6b763df1c9c1980d
Hopson97 <mhopson@hotmail.co.uk>
2018-12-25 16:15:41 +0000

    Make filewriter use the new bufferwriter class

--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -2,8 +2,12 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 #include "CYObject.h"
+#include "BinaryFile.h"
+#include "Benchmark.h"
 
 namespace {
     enum class PType : uint8_t {
@@ -16,84 +20,34 @@ namespace {
         Unknown     = 250,
     };
 
-    template<typename WriteType, typename OrigType>
-    void writeNumber(std::ofstream& outFile, OrigType n) {
-        WriteType converted = static_cast<WriteType>(n);
-        outFile.write(reinterpret_cast<const char*>(&converted), sizeof(converted));
-    }
-
-    void writeString(std::ofstream& outFile, const std::string& string) {
-        auto size = (uint16_t)string.size();
-        writeNumber<uint16_t>(outFile, size);
-        outFile.write(reinterpret_cast<const char*>(&size), sizeof(size));
-        outFile.write(string.c_str(), string.size());
-    }
-
-    void writeObjectTypeHeader(std::ofstream& outFile, const std::initializer_list<PType>& format, ObjectID id, size_t numObjects) {
-        writeNumber<uint8_t>(outFile, id);
-        writeNumber<uint8_t>(outFile, format.size());
-        for (auto p : format) {
-            writeNumber<uint8_t>(outFile, p);
-        }
-        
-        writeNumber<uint32_t>(outFile, numObjects);
-    }
-
-    void writePosition(std::ofstream& outFile, const Position& position) {
-        auto x = position.x;
-        auto z = position.z;
-
-        writeNumber<int16_t>(outFile, x);
-        writeNumber<int16_t>(outFile, z);
-    }
-
-    void writeSingleObjectHeader(std::ofstream& outFile, const Position& position, uint8_t floor) {
-        writePosition(outFile, position);
-        writeNumber<uint8_t>(outFile, floor);
-    }
-
-    void writeWalls(std::ofstream& outFile, const std::vector<CYWall>& walls) {
-        writeObjectTypeHeader(
-            outFile, 
-            {PType::Texture, PType::Texture, PType::QValue}, 
-            ObjectID::Wall, walls.size());
-
-        for (const auto& wall : walls) {
-            writePosition(outFile, wall.beginPoint);
-            writePosition(outFile, wall.endPoint);
-            writeNumber<uint8_t>(outFile, wall.floor);
-            writeNumber<uint32_t>(outFile, std::stoi(wall.properties[0]));
-            writeNumber<uint32_t>(outFile, std::stoi(wall.properties[1]));
-            writeNumber<uint8_t>(outFile, std::stoi(wall.properties[2]));
+    void writeObjectTypeHeader(BinaryFileBuffer& buffer, ObjectID id, size_t numObjects, const std::initializer_list<PType>& format) {
+        buffer << (u8)id << (u8)format.size() << (u32)numObjects;
+        for (auto& property : format) {
+            buffer << (u8)property;
         }
     }
 
-    void writeFloors(std::ofstream& outFile, const std::vector<CYFloor>& floors) {
-        writeObjectTypeHeader(
-            outFile,
-            {PType::Texture, PType::IsHidden, PType::Texture},
-            ObjectID::Floor, floors.size());
-        for (const auto& floor : floors) {
-            writePosition(outFile, floor.vertexA);
-            writePosition(outFile, floor.vertexA);
-            writePosition(outFile, floor.vertexA);
-            writePosition(outFile, floor.vertexA);
-        }
+    void writeSingleObjectHeader(BinaryFileBuffer& buffer, const Position& position, uint8_t floor) {
+        buffer << position << (u8)floor;
     }
+    
 }
 
-
 void writeLevelBinary(const CYLevel& level, const std::string& fileName) {
-    std::ofstream outfile(fileName, std::ofstream::binary);
+    BinaryFileBuffer bBuffer;
+    bBuffer << (u8)1 
+            << level.name << level.creator 
+            << (u8)std::stoi(level.numFloors)
+            << (u8)level.backmusic << (u8)level.theme << (u8)level.weather;
 
-    writeNumber<uint16_t>(outfile, 1); //Version number
-    writeString(outfile, level.name);
-    writeString(outfile, level.creator);
-    writeNumber<uint8_t>(outfile, std::stoi(level.numFloors));
-    writeNumber<uint8_t>(outfile, level.backmusic);
-    writeNumber<uint8_t>(outfile, level.theme);
-    writeNumber<uint8_t>(outfile, level.weather);
-
-    writeWalls(outfile, level.walls);
-    writeFloors(outfile, level.floors);
+    //WALLS
+    writeObjectTypeHeader(bBuffer, ObjectID::Wall, level.walls.size(), 
+        {PType::Texture, PType::Texture, PType::QValue});
+    for (const auto& wall : level.walls) {
+        bBuffer << wall;
+    } 
+            
+    //Final output
+    std::ofstream outfile3(fileName + "_v2", std::ofstream::binary);
+    outfile3.write(bBuffer.getBuffer(), bBuffer.bufferSize());
 }
